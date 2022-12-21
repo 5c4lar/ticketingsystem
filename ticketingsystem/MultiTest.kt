c5c77535b2f4445b11509d3edf5cdf5bb693a484
ucasqsl <qinsiliang18@mails.ucas.ac.cn>
2022-12-22 03:56:21 +0800

    save results for plot

--- ticketingsystem/MultiTest.kt
+++ ticketingsystem/MultiTest.kt
@@ -1,7 +1,9 @@
 package ticketingsystem
 
+import java.util.concurrent.TimeUnit
+
 fun main(args: Array<String>) {
-  val threadNums = arrayOf(1, 2, 4, 8, 16, 32, 64)
+  val threadNums = arrayOf(1, 2, 4, 8, 16, 32)
   if (args.size != 3) {
     println("The arguments of Test is testNum, benchNum, warmUpNum")
     return
@@ -10,7 +12,31 @@ fun main(args: Array<String>) {
   val benchNum = args[1].toInt()
   val warmUpNum = args[2].toInt()
   Test.readConfig("TrainConfig")
+  val results: MutableMap<Int, Test.Result> = HashMap()
   for (threadNum in threadNums) {
-    Test.testWithConfig(threadNum, testNum, benchNum, warmUpNum)
+    val res = Test.testWithConfig(threadNum, testNum, benchNum, warmUpNum)
+    results[threadNum] = res
   }
+  println("========================================")
+  println("ThreadNum,Total Calls,Total Time,Mean Throughput,Refund Calls,Refund Time,Refund Latency,Buy Calls,Buy Time,Buy Latency,Inquiry Calls,Inquiry Time,Inquiry Latency")
+  for (threadNum in threadNums) {
+    val res = results[threadNum]
+    for (i in 0 until benchNum) {
+      val ms = TimeUnit.NANOSECONDS.toMillis(res!!.times[i])
+      val meanThroughputs = res.totalCalls[i].toDouble() / ms
+      val eachCallCount = res.eachCallCount[i]
+      val eachCallTime = res.eachCallTime[i]
+      val refundCalls = eachCallCount[0]
+      val refundTime = TimeUnit.NANOSECONDS.toMillis(eachCallTime[0])
+      val refundLatency = TimeUnit.NANOSECONDS.toMicros(eachCallTime[0]) / eachCallCount[0].toDouble()
+      val buyCalls = eachCallCount[1]
+      val buyTime = TimeUnit.NANOSECONDS.toMillis(eachCallTime[1])
+      val buyLatency = TimeUnit.NANOSECONDS.toMicros(eachCallTime[1]) / eachCallCount[1].toDouble()
+      val inquiryCalls = eachCallCount[2]
+      val inquiryTime = TimeUnit.NANOSECONDS.toMillis(eachCallTime[2])
+      val inquiryLatency = TimeUnit.NANOSECONDS.toMicros(eachCallTime[2]) / eachCallCount[2].toDouble()
+      println("$threadNum,${res.totalCalls[i]},$ms,$meanThroughputs, $refundCalls, $refundTime, $refundLatency, $buyCalls, $buyTime, $buyLatency, $inquiryCalls, $inquiryTime, $inquiryLatency")
+    }
+  }
+  println("========================================")
 }
