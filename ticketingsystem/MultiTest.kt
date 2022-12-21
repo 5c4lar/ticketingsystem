0b65115dc5aad61f08012e08b649f0b5b3113d2b
ucasqsl <qinsiliang18@mails.ucas.ac.cn>
2022-12-21 12:15:55 +0800

    warmup multitest

--- ticketingsystem/MultiTest.kt
+++ ticketingsystem/MultiTest.kt
@@ -32,7 +32,7 @@ fun main() {
   val threadNums = arrayOf(1, 2, 4, 8, 16, 32, 64)
   val testNum = 1000000
   val benchNum = 1
-  val warmUpNum = 0
+  val warmUpNum = 1
   for (threadNum in threadNums) {
     println("threadNum: $threadNum")
     testWithConfig(threadNum, testNum, benchNum, warmUpNum)
